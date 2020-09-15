1123a342dd8f52ffbf74008893054f6dc2122eeb
spfxeprimaverajr <57431452+spfxeprimaverajr@users.noreply.github.com>
2020-09-15 21:39:40 +0800

    SPFX-2 #for-dev #comment test

---
+++ src/webparts/mytestgit/components/Mytestgit.tsx
@@ -0,0 +1,25 @@
+import * as React from 'react';
+import styles from './Mytestgit.module.scss';
+import { IMytestgitProps } from './IMytestgitProps';
+import { escape } from '@microsoft/sp-lodash-subset';
+
+export default class Mytestgit extends React.Component<IMytestgitProps, {}> {
+  public render(): React.ReactElement<IMytestgitProps> {
+    return (
+      <div className={ styles.mytestgit }>
+        <div className={ styles.container }>
+          <div className={ styles.row }>
+            <div className={ styles.column }>
+              <span className={ styles.title }>Welcome to SharePoint!</span>
+              <p className={ styles.subTitle }>Customize SharePoint experiences using Web Parts.</p>
+              <p className={ styles.description }>{escape(this.props.description)}</p>
+              <a href="https://aka.ms/spfx" className={ styles.button }>
+                <span className={ styles.label }>Learn more</span>
+              </a>
+            </div>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
---
+++ src/webparts/mytestgit/loc/mystrings.d.ts
@@ -0,0 +1,10 @@
+declare interface IMytestgitWebPartStrings {
+  PropertyPaneDescription: string;
+  BasicGroupName: string;
+  DescriptionFieldLabel: string;
+}
+
+declare module 'MytestgitWebPartStrings' {
+  const strings: IMytestgitWebPartStrings;
+  export = strings;
+}
