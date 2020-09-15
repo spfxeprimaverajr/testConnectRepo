6bebd5ac75a38873496942b6b438568d86d54cbc
spfxeprimaverajr <57431452+spfxeprimaverajr@users.noreply.github.com>
2020-09-15 22:21:05 +0800

    @SPFX-3 #for-dev #comment this is testing transition

--- src/webparts/mytestgit/components/Mytestgit.tsx
+++ src/webparts/mytestgit/components/Mytestgit.tsx
@@ -6,15 +6,15 @@ import { escape } from '@microsoft/sp-lodash-subset';
 export default class Mytestgit extends React.Component<IMytestgitProps, {}> {
   public render(): React.ReactElement<IMytestgitProps> {
     return (
-      <div className={ styles.mytestgit }>
-        <div className={ styles.container }>
-          <div className={ styles.row }>
-            <div className={ styles.column }>
-              <span className={ styles.title }>Welcome to SharePoint!</span>
-              <p className={ styles.subTitle }>Customize SharePoint experiences using Web Parts.</p>
-              <p className={ styles.description }>{escape(this.props.description)}</p>
-              <a href="https://aka.ms/spfx" className={ styles.button }>
-                <span className={ styles.label }>Learn more</span>
+      <div className={styles.mytestgit}>
+        <div className={styles.container}>
+          <div className={styles.row}>
+            <div className={styles.column}>
+              <span className={styles.title}>Welcome to SharePoint Test!</span>
+              <p className={styles.subTitle}>Customize SharePoint experiences using Web Parts.</p>
+              <p className={styles.description}>{escape(this.props.description)}</p>
+              <a href="https://aka.ms/spfx" className={styles.button}>
+                <span className={styles.label}>Learn more</span>
               </a>
             </div>
           </div>
