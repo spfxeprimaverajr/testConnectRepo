86567d4e07713a86b8476cc6388b8559b9758852
spfxeprimaverajr <57431452+spfxeprimaverajr@users.noreply.github.com>
2020-09-16 14:27:51 +0800

    SPFX-3 #for-testing #comment issue is fixed and ready for testing

--- src/webparts/mytestgit/components/Mytestgit.tsx
+++ src/webparts/mytestgit/components/Mytestgit.tsx
@@ -10,7 +10,7 @@ export default class Mytestgit extends React.Component<IMytestgitProps, {}> {
         <div className={styles.container}>
           <div className={styles.row}>
             <div className={styles.column}>
-              <span className={styles.title}>Welcome to SharePoint Test 222git s adding new test!</span>
+              <span className={styles.title}>This is now good for TESTING</span>
               <p className={styles.subTitle}>Customize SharePoint experiences using Web Parts.</p>
               <p className={styles.description}>{escape(this.props.description)}</p>
               <a href="https://aka.ms/spfx" className={styles.button}>
